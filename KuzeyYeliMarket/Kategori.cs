a5eb194ac65d0a8a1711a225106aa2a486fcc03b
burcMnt <burcumantar0@gmail.com>
2021-06-25 17:15:43 +0300

    Add project files.

---
+++ KuzeyYeliMarket/Kategori.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuzeyYeliMarket
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string KategoriAd { get; set; }
+        public int? UstKategoriId { get; set; }
+
+     }
+}
---
+++ KuzeyYeliMarket/KategoriForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace KuzeyYeliMarket
+{
+    partial class KategoriForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKategoriAd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboUstKategori = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(143, 14);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(148, 30);
+            this.txtId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(156, 156);
+            this.btnIptal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(112, 35);
+            this.btnIptal.TabIndex = 5;
+            this.btnIptal.Text = "İptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(21, 156);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(112, 35);
+            this.btnKaydet.TabIndex = 4;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kategori Adı";
+            // 
+            // txtKategoriAd
+            // 
+            this.txtKategoriAd.Location = new System.Drawing.Point(142, 50);
+            this.txtKategoriAd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtKategoriAd.Name = "txtKategoriAd";
+            this.txtKategoriAd.Size = new System.Drawing.Size(148, 30);
+            this.txtKategoriAd.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 93);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Üst Kategori";
+            // 
+            // cboUstKategori
+            // 
+            this.cboUstKategori.DisplayMember = "KategoriAd";
+            this.cboUstKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboUstKategori.FormattingEnabled = true;
+            this.cboUstKategori.Location = new System.Drawing.Point(142, 90);
+            this.cboUstKategori.Name = "cboUstKategori";
+            this.cboUstKategori.Size = new System.Drawing.Size(149, 33);
+            this.cboUstKategori.TabIndex = 7;
+            this.cboUstKategori.ValueMember = "Id";
+            // 
+            // KategoriForm
+            // 
+            this.AcceptButton = this.btnKaydet;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 213);
+            this.Controls.Add(this.cboUstKategori);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKategoriAd);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnIptal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "KategoriForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Yeni Kategori";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKategoriAd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboUstKategori;
+    }
+}
